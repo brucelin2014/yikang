bb97deb96c5a0eec69d3aca5e0a87c0d0dcbdac7
linchunmeng <linchunmeng511@163.com>
2021-01-07 16:02:01 +0800

    数据按最后修改时间排序

--- uniCloud-aliyun/cloudfunctions/get_list_bug/index.js
+++ uniCloud-aliyun/cloudfunctions/get_list_bug/index.js
@@ -23,7 +23,7 @@ exports.main = async (event, context) => {
 	}
 
 	//查询指定字段的一条记录
-	let res = await collection.where(filter).skip((pageIndex - 1) * pageSize).limit(pageSize).orderBy("last_modified_date", "asc").get(); // asc / desc
+	let res = await collection.where(filter).skip((pageIndex - 1) * pageSize).limit(pageSize).orderBy("last_modified_date", "desc").get(); // asc / desc
 	//console.log("get " + JSON.stringify(res));
 
 	//返回数据给客户端
